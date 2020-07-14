1df7e2649052b703daeef0ce2fd819d894d7b208
Austin <22li43@lexingtonma.org>
2020-07-14 09:45:00 -0400

    BlueThreshold updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AvgPixelColorOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AvgPixelColorOpMode.java
@@ -34,16 +34,15 @@ import java.util.List;
  *YES
  */
 @Config
-@Autonomous(name= "testCopy")
+@Autonomous
 public class AvgPixelColorOpMode extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
 
     //0 means skystone, 1 means yellow stone
     //-1 for debug, but we can keep it like this because if it works, it should change to either 0 or 255
     private static int valMid = -1;
-    private static int valLeft = -1;
-    private static int valRight = -1;
 
+    private static final int yellowThreshold = 113;
     private static float rectHeight = 1.5f / 8f;
     private static float rectWidth = .6f / 8f;
 
@@ -51,8 +50,7 @@ public class AvgPixelColorOpMode extends LinearOpMode {
     private static float offsetY = 0f / 8f;//changing this moves the three rects and circles up or down, range: (-4, 4) not inclusive
 
     private static float[] midPos = {4f / 8f + offsetX, 4f / 8f + offsetY};//0 = col, 1 = row
-    private static float[] leftPos = {4f / 8f + offsetX, 2f / 8f + offsetY};
-    private static float[] rightPos = {4f / 8f + offsetX, 6f / 8f + offsetY};
+
     //moves all rectangles right or left by amount. units are in ratio to monitor
 
     private final int rows = 640;
@@ -75,7 +73,7 @@ public class AvgPixelColorOpMode extends LinearOpMode {
         runtime.reset();
         //all of our movement jazz
         while (opModeIsActive()) {
-            telemetry.addData("Values", valLeft+"   "+valMid+"   "+valRight);
+            telemetry.addData("Values", valMid);
             telemetry.addData("Height", rows);
             telemetry.addData("Width", cols);
 
@@ -138,7 +136,7 @@ public class AvgPixelColorOpMode extends LinearOpMode {
             Core.extractChannel(yCbCrChan2Mat, yCbCrChan2Mat, 2);//takes cb difference and stores
 
             //b&w
-            Imgproc.threshold(yCbCrChan2Mat, thresholdMat, 102, 255, Imgproc.THRESH_BINARY_INV);
+            Imgproc.threshold(yCbCrChan2Mat, thresholdMat, yellowThreshold, 255, Imgproc.THRESH_BINARY_INV);
 
             //outline/contour
             Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
@@ -147,38 +145,31 @@ public class AvgPixelColorOpMode extends LinearOpMode {
 
 
             //get values from frame
-            double[] pixMid = thresholdMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0]));//gets value at circle
-            System.out.println(Arrays.toString(pixMid));
-            valMid = (int)pixMid[0];
 
-            double[] pixLeft = thresholdMat.get((int)(input.rows()* leftPos[1]), (int)(input.cols()* leftPos[0]));//gets value at circle
-            System.out.println(Arrays.toString(pixLeft));
-            valLeft = (int)pixLeft[0];
+            double[][] averagePixMid = {
+                    thresholdMat.get((int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0])), // center
+                    thresholdMat.get(1+ (int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0])),  //below 1
+                    thresholdMat.get(-1 + (int)(input.rows()* midPos[1]), (int)(input.cols()* midPos[0])), //above 1
+                    thresholdMat.get((int)(input.rows()* midPos[1]),1+  (int)(input.cols()* midPos[0])), // right 1
+                    thresholdMat.get((int)(input.rows()* midPos[1]), -1 + (int)(input.cols()* midPos[0]))//left 1
 
-            double[] pixRight = thresholdMat.get((int)(input.rows()* rightPos[1]), (int)(input.cols()* rightPos[0]));//gets value at circle
-            System.out.println(Arrays.toString(pixRight));
-            valRight = (int)pixRight[0];
+            };
+            valMid = (int)averagePixMid[0][0];
+            for(int i = 1; i < averagePixMid.length; i++ ){
+                valMid += (int)averagePixMid[i][0];
+            }
+            valMid /= averagePixMid.length;
 
             //create three points
             Point pointMid = new Point((int)(input.cols()* midPos[0]), (int)(input.rows()* midPos[1]));
-            Point pointLeft = new Point((int)(input.cols()* leftPos[0]), (int)(input.rows()* leftPos[1]));
-            Point pointRight = new Point((int)(input.cols()* rightPos[0]), (int)(input.rows()* rightPos[1]));
+
 
             //draw circles on those points
             Imgproc.circle(all, pointMid,5, new Scalar( 255, 0, 0 ),1 );//draws circle
-            Imgproc.circle(all, pointLeft,5, new Scalar( 255, 0, 0 ),1 );//draws circle
-            Imgproc.circle(all, pointRight,5, new Scalar( 255, 0, 0 ),1 );//draws circle
+
 
             //draw 3 rectangles
-            Imgproc.rectangle(//1-3
-                    all,
-                    new Point(
-                            input.cols()*(leftPos[0]-rectWidth/2),
-                            input.rows()*(leftPos[1]-rectHeight/2)),
-                    new Point(
-                            input.cols()*(leftPos[0]+rectWidth/2),
-                            input.rows()*(leftPos[1]+rectHeight/2)),
-                    new Scalar(0, 255, 0), 3);
+
             Imgproc.rectangle(//3-5
                     all,
                     new Point(
@@ -188,15 +179,7 @@ public class AvgPixelColorOpMode extends LinearOpMode {
                             input.cols()*(midPos[0]+rectWidth/2),
                             input.rows()*(midPos[1]+rectHeight/2)),
                     new Scalar(0, 255, 0), 3);
-            Imgproc.rectangle(//5-7
-                    all,
-                    new Point(
-                            input.cols()*(rightPos[0]-rectWidth/2),
-                            input.rows()*(rightPos[1]-rectHeight/2)),
-                    new Point(
-                            input.cols()*(rightPos[0]+rectWidth/2),
-                            input.rows()*(rightPos[1]+rectHeight/2)),
-                    new Scalar(0, 255, 0), 3);
+
 
             switch (stageToRenderToViewport)
             {
