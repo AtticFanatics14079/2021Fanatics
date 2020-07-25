2c6a27656bf2410e63c6517c8d19adf106356155
JonCoombs <22coombs2@lexingtonma.org>
2020-07-25 08:27:50 -0400

    Me make contours.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Contours.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Config
+@Autonomous
+public class Contours extends LinearOpMode {
+
+    private final int rows = 640;
+    private final int cols = 480;
+
+    OpenCvCamera phoneCam;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam.openCameraDevice();//open camera
+        phoneCam.setPipeline(new StageSwitchingPipeline());//different stages
+        phoneCam.startStreaming(rows, cols, OpenCvCameraRotation.UPRIGHT);//display on RC
+        //width, height
+        //width = height in this case, because camera is in portrait mode.
+
+        waitForStart();
+        //all of our movement jazz
+        while (opModeIsActive()) {
+            telemetry.addData("Height", rows);
+            telemetry.addData("Width", cols);
+
+            telemetry.update();
+            sleep(100);
+        }
+    }
+
+    //detection pipeline
+    static class StageSwitchingPipeline extends OpenCvPipeline
+    {
+        Mat rawMat = new Mat();
+        Mat grayMat = new Mat();
+        Mat hierarchy = new Mat();
+        Mat contoursMat = new Mat();
+
+        enum Stage
+        {
+            RAW,
+            COOKED
+        }
+
+        private StageSwitchingPipeline.Stage stageToRenderToViewport = StageSwitchingPipeline.Stage.RAW;
+        private StageSwitchingPipeline.Stage[] stages = StageSwitchingPipeline.Stage.values();
+
+        @Override
+        public void onViewportTapped()
+        {
+
+            /*
+             * Note that this method is invoked from the UI thread
+             * so whatever we do here, we must do quickly.
+             */
+
+            int currentStageNum = stageToRenderToViewport.ordinal();
+
+            int nextStageNum = currentStageNum + 1;
+
+            if(nextStageNum >= stages.length)
+            {
+                nextStageNum = 0;
+            }
+
+            stageToRenderToViewport = stages[nextStageNum];
+        }
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            rawMat = input;
+            Imgproc.GaussianBlur(rawMat, grayMat, new Size(3,3),0,0);
+            Imgproc.cvtColor(grayMat, grayMat, Imgproc.COLOR_BGR2GRAY);
+            Imgproc.Canny(grayMat, contoursMat, 50, 150, 3, false);
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(contoursMat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            Mat horizontalStructure = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5));
+            Imgproc.dilate(contoursMat, contoursMat, horizontalStructure);
+
+            Mat drawing = Mat.zeros(contoursMat.size(), CvType.CV_8UC3);
+            for (int i = 0; i < contours.size(); i++) {
+                Scalar color = new Scalar(0, 255, 255);
+                Imgproc.drawContours(drawing, contours, i, color, 2, Imgproc.LINE_8, hierarchy, 0, new Point());
+            }
+
+            switch (stageToRenderToViewport)
+            {
+                case COOKED:
+                {
+                    return drawing;
+                }
+                default:
+                {
+                    return input;
+                }
+            }
+        }
+
+    }
+
+
+
+}
