2ed7030187a31a118fcab34a61a1f18d000dc3a4
Anthony <anthonyrzhao@gmail.com>
2020-08-21 10:26:36 -0400

    Servo Thing OpMode Thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LimitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LimitTest.java
@@ -12,9 +12,28 @@ public class LimitTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         LimitConfiguration config = new LimitConfiguration();
         config.Configure(hardwareMap);
+        double servoPos = 0.5;
+        boolean direction = true;
+        boolean pressed = false;
         while(!isStopRequested()) {
-            telemetry.addData("PLOX WORK: ", config.isPressed1());
+            if((config.isPressed1() || config.isPressed2()) && !pressed){
+                direction = !direction;
+                pressed = true;
+            }
+            else if (!config.isPressed1() && !config.isPressed2()){
+                pressed = false;
+            }
+            if(direction){
+                servoPos += 0.01;
+            }
+            else{
+                servoPos -= 0.01;
+            }
+            config.servo.setPosition(servoPos);
+            telemetry.addData("Limit1: ", config.isPressed1());
+            telemetry.addData("Limit2: ", config.isPressed2());
             telemetry.update();
+            sleep(10);
         }
     }
 }
