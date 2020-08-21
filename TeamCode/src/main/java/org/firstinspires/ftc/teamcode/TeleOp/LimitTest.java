fc835cad2c1eb289b76c0a4b5db9f91ae3c19fa4
JonCoombs <22coombs2@lexingtonma.org>
2020-08-21 13:44:52 -0400

    Number change.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LimitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LimitTest.java
@@ -12,10 +12,11 @@ public class LimitTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         LimitConfiguration config = new LimitConfiguration();
         config.Configure(hardwareMap);
-        double servoPos = 0.5;
+        double servoPos = 0.8;
         boolean direction = true;
         boolean pressed = false;
         while(!isStopRequested()) {
+
             if((config.isPressed1() || config.isPressed2()) && !pressed){
                 direction = !direction;
                 pressed = true;
@@ -23,16 +24,22 @@ public class LimitTest extends LinearOpMode {
             else if (!config.isPressed1() && !config.isPressed2()){
                 pressed = false;
             }
+
             if(direction){
                 servoPos += 0.01;
             }
             else{
                 servoPos -= 0.01;
             }
+
             config.servo.setPosition(servoPos);
             telemetry.addData("Limit1: ", config.isPressed1());
             telemetry.addData("Limit2: ", config.isPressed2());
+            telemetry.addData("Pressed: ", pressed);
+            telemetry.addData("Servo Position: ", config.servo.getPosition());
             telemetry.update();
+
+            //Goal is to move slower than normal to avoid risking the limit switches
             sleep(10);
         }
     }
